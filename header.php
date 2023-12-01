ad3340135ae32c6416dd26f744060e4735b13904
Kanda Yuki <kand4y@gmail.com>
2023-12-02 03:25:13 +0900

    add: headerを追加

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <?php
 
+$directory_uri = get_template_directory_uri();
+
 ?>
 
 <!DOCTYPE html>
@@ -8,12 +10,30 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script>
+    !(function() {
+      const viewport = document.querySelector('meta[name="viewport"]');
 
+      function switchViewport() {
+        const value =
+          window.outerWidth > 360 ?
+          "width=device-width,initial-scale=1" :
+          "width=360";
+        if (viewport.getAttribute("content") !== value) {
+          viewport.setAttribute("content", value);
+        }
+      }
+      addEventListener("resize", switchViewport, false);
+      switchViewport();
+    })();
+  </script>
   <?php wp_head() ?>
+
+  <script type="module" crossorigin src="<?= $directory_uri ?>/assets/index-8YyaKEi5.js"></script>
 </head>
 
 <body class="bg-white">
   <div class="wrapper">
-    <?php get_template_part('includes/footer-part') ?>
+    <?php get_template_part('includes/header-part') ?>
 
     <main>
