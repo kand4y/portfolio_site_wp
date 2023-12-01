f847da04efa4b581718364302accb8d6a54f1126
Kanda Yuki <kand4y@gmail.com>
2023-12-02 03:25:30 +0900

    add: 仮でコンタクトセクションを追加

---
+++ sections/front_contact.php
@@ -0,0 +1,45 @@
+<?php
+
+?>
+
+<section id="contact">
+  <div class="bg-white">
+    <div class="px-4">
+      <div class="mx-auto max-w-2xl">
+        <div class="py-24">
+          <div class="flex items-center justify-center pb-12">
+            <h2 class="text-center">Contact</h2>
+          </div>
+
+          <div class="flex items-center justify-center pb-12">
+            <p class="break-keep text-center">
+              まずは気軽にご相談ください。
+            </p>
+          </div>
+
+          <form action="">
+            <div class="flex flex-col justify-center gap-8">
+              <div>
+                <label class="mb-1 block" for="">お名前</label>
+                <input type="text" />
+              </div>
+
+              <div>
+                <label class="mb-1 block" for="">メールアドレス</label>
+                <input type="text" />
+              </div>
+
+              <div>
+                <label class="mb-1 block" for="">お問い合わせ内容</label>
+                <textarea name="" id="" rows="10"></textarea>
+              </div>
+            </div>
+            <div class="flex items-center justify-center pt-12">
+              <input type="submit" value="送信" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
