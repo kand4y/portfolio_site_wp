3657efbe7452509d89d77fb28a6bd85328bac7ad
Kanda Yuki <kand4y@gmail.com>
2023-12-02 03:17:08 +0900

    add: footerを追加

--- footer.php
+++ footer.php
@@ -1,15 +1,4 @@
 </main>
-<footer>
-  <div class="bg-gray">
-    <div class="px-4">
-      <div class="py-12">
-        <div class="flex justify-center items-center">
-          <small class="text-white">footer</small>
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
 
 <?php get_template_part('includes/footer-part') ?>
 </div>
---
+++ includes/footer-part.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<footer>
+  <div class="bg-gray">
+    <div class="px-4">
+      <div class="py-12">
+        <div class="flex items-center justify-center">
+          <small class="text-white">&copy; kand4y.</small>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
